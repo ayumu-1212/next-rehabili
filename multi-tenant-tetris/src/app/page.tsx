8a17acdb41dfd19ca48a158fa8b69de14e97e4e3
ayumu-1212 <ayumuabe1434@gmail.com>
2024-08-11 09:54:47 +0000

    update: tetrisFieldを分離

--- multi-tenant-tetris/src/app/page.tsx
+++ multi-tenant-tetris/src/app/page.tsx
@@ -1,7 +1,7 @@
 const HEIGHT = 20;
 const WIDTH = 10;
 
-export default function Home() {
+const tetrisField = () => {
   const rowBlocks = Array.from({ length: WIDTH }, (_, i) => (
     <div key={i} className="w-8 h-8 bg-[#1d2731] border border-[#1d2731]"></div>
   ));
@@ -12,11 +12,17 @@ export default function Home() {
   ));
 
   return (
+    <div className="bg-[#1d2731] flex flex-col w-80 border-2 border-[#4881C6]">
+      {blocks}
+    </div>
+  );
+}
+
+export default function Home() {
+  return (
     <main className="min-h-screen p-12">
       <div className="p-12 w-full flex justify-center">
-        <div className="bg-[#1d2731] flex flex-col w-80">
-          {blocks}
-        </div>
+        {tetrisField()}
       </div>
     </main>
   );
