8aa8f96ecdb46fd674b7576efebb82632f168482
ayumu-1212 <ayumuabe1434@gmail.com>
2024-08-12 06:08:35 +0000

    refactor: Separate tetrisField logic and optimize rendering

--- multi-tenant-tetris/src/app/page.tsx
+++ multi-tenant-tetris/src/app/page.tsx
@@ -1,5 +1,5 @@
 "use client";
-import { useEffect, useState } from "react";
+import { useEffect, useState, useRef } from "react";
 
 const HEIGHT = 20;
 const WIDTH = 10;
@@ -9,20 +9,19 @@ type Address = {
   y: number;
 }
 
-const tetrisField = (existBlock: Address) => {
-  const rowBlocks = (y: number) => {
-    return Array.from({ length: WIDTH }, (_, x) => {
-      if (existBlock.x === x && existBlock.y === y) {
-        return <div key={'x-' + x} className="w-8 h-8 bg-[#4881C6] border border-[#1d2731]"></div>;
-      }
-      return <div key={'x-' + x} className="w-8 h-8 bg-[#1d2731] border border-[#1d2731]"></div>;
-    });
-  };
-  const blocks = Array.from({ length: HEIGHT }, (_, y) => (
-    <div key={'y-' + y} className="flex justify-center">
-      {rowBlocks(y)}
-    </div>
-  ));
+const tetrisField = (existBlocks: Boolean[][]) => {
+  const blocks = existBlocks.map((row, y) => {
+    return (
+      <div key={'y-' + y} className="flex justify-center">
+        {row.map((block, x) => {
+          if (block) {
+            return <div key={'x-' + x} className="w-8 h-8 bg-[#4881C6] border border-[#1d2731]"></div>;
+          }
+          return <div key={'x-' + x} className="w-8 h-8 bg-[#1d2731] border border-[#1d2731]"></div>;
+        })}
+      </div>
+    );
+  });
 
   return (
     <div className="bg-[#1d2731] flex flex-col w-80 border-2 border-[#4881C6]">
@@ -34,6 +33,14 @@ const tetrisField = (existBlock: Address) => {
 export default function Home() {
 
   const [address, setAddress] = useState<Address>({ x: 4, y: 0 });
+  const [existBlocks, setExistBlocks] = useState<Boolean[][]>(Array.from({ length: HEIGHT }, () => Array.from({ length: WIDTH }, () => false)));
+
+  const addressRef = useRef(address);
+
+  useEffect(() => {
+    addressRef.current = address;
+  }, [address]);
+
 
   useEffect(() => {
     const interval = setInterval(() => {
@@ -43,15 +50,32 @@ export default function Home() {
         }
         return { ...prev, y: prev.y + 1 };
       });
+      setExistBlocks((prev) => {
+        const newBlocks = prev.map((row, y) => {
+          return row.map((block, x) => {
+            if (x == addressRef.current.x && y == addressRef.current.y) {
+              return true;
+            }
+            else if (x == addressRef.current.x && y == addressRef.current.y - 1) {
+              return false;
+            }
+            return block;
+          });
+        });
+        return newBlocks;
+      }
+      );
+
     }, 1000);
     return () => clearInterval(interval); // クリーンアップ関数でタイマーをクリア
   }, []);
 
 
+
   return (
     <main className="min-h-screen p-12">
       <div className="p-12 w-full flex justify-center">
-        {tetrisField(address)}
+        {tetrisField(existBlocks)}
       </div>
     </main>
   );
